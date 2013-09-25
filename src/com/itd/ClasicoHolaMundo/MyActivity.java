c3755f2d7a33ca2280591a8d60e1c60da92f99a0
Roberto Lopez <jrlopez@gmail.com>
2013-09-24 19:31:47 -0500

    Abrir nueva actividad correctamente

--- src/com/itd/ClasicoHolaMundo/MyActivity.java
+++ src/com/itd/ClasicoHolaMundo/MyActivity.java
@@ -1,6 +1,7 @@
 package com.itd.ClasicoHolaMundo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,7 +22,8 @@ public class MyActivity extends Activity {
         btnEnviar.setOnClickListener(new View.OnClickListener(){
             @Override
              public void onClick(View v){
-                    texto.setText("Pa que le puchabas juan!!!");
+                Intent intent = new Intent(MyActivity.this, nueva_ventana.class);
+                startActivity(intent);
             }
         });
 
